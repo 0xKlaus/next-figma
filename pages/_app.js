77c53781c3ddb5f58f8ee5cbc9a003b7612d16f1
Hideki815 <moriyamahideki815@gmail.com>
2021-11-25 21:35:30 +0900

    Create Next Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>AAVE</title>
+        <link rel="icon" href="icon/aave2.png" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
